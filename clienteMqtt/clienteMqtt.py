264bc4b55263bfefd3ed913556c3d7ef13ab9269
Sebastian Techeira <franchu0802@gmail.com>
2026-05-05 21:03:35 -0300

    Implementacion de logica inicial, esta incompleto

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -3,20 +3,49 @@ import aiomqtt
 
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
-async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.verify_mode = ssl.CERT_REQUIRED
-    tls_context.check_hostname = True
-    tls_context.load_default_certs()
+class Estado():
+    def __init__(self,valor):
+        self.valor = 0
 
-    async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
-        port=8883,
-        tls_context=tls_context,
-    ) as client:
-        await client.subscribe(os.environ['TOPICO'])
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+async def contador(client,n):
+
+    while True:
+        n.valor += 1
+        await asyncio.sleep(3)
+
+async def publicar_contador(client, n):
+
+    while True:
+        try:
+            await client.publish(os.environ["TOPICO1"],
+                            n.valor, 
+                            qos=aiomqtt.QoS.AT_LEAST_ONCE)
+            await asyncio.sleep(5)
+        except:
+            pass
+
+
+async def main():
+
+    try:
+        cont = Estado()
+        tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        tls_context.verify_mode = ssl.CERT_REQUIRED
+        tls_context.check_hostname = True
+        tls_context.load_default_certs()
+
+        async with aiomqtt.Client(
+            os.environ['SERVIDOR'],
+            port=8883,
+            tls_context=tls_context,
+        ) as client:
+            await client.subscribe(os.environ['TOPICO1'])
+            await client.subscribe(os.environ['TOPICO2'])
+            asyncio.create_task(contador(client,cont.value), name="contador")
+            asyncio.create_task(publicar_contador(client, cont), name="publicar contador")
+            
+    except KeyboardInterrupt:
+        print("Programa finalizado")
 
 if __name__ == "__main__":
     asyncio.run(main())
